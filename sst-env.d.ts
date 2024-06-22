ede0a3055ae397dd4664578966e65a191a4381fc
Jared Gore <jared@madstone.io>
2024-06-22 10:29:33 -0500

    boom

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -1,1 +1,24 @@
-/// <reference path="./.sst/types.generated.ts" />
+/* tslint:disable */
+/* eslint-disable */
+import "sst"
+declare module "sst" {
+  export interface Resource {
+    GuestbookEntries: {
+      name: string
+      type: "sst.aws.Dynamo"
+    }
+    GuestbookNextJS: {
+      type: "sst.aws.Nextjs"
+      url: string
+    }
+    IPInfoToken: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    ParentZoneId: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+  }
+}
+export {}
